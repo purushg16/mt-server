3664fb7d76a5b30f657f523ceaefb5d7f65b6151
Purushothaman G <macdasys@gmail.com>
2023-11-24 12:23:36 +0530

    python-path /opt/render/.local/lib/python3.7/

--- App.js
+++ App.js
@@ -23,9 +23,10 @@ const storage =
 });
 
 console.log(__dirname);
-const pythonPath = '/usr/bin/python3'
+const pythonPath = '/opt/render/.local/lib/python3.7/'
 // const pythonPath = path.join(__dirname, 'usr/bin/python3')
-
+// /opt/render/.local/lib/python3.7/site-packages
+// /usr/bin/python3
 const upload =
     multer({
         storage,
