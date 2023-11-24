6e044bbe8be16b170e7e94c586c266403a8a2660
Purushothaman G <macdasys@gmail.com>
2023-11-24 12:20:56 +0530

    python-path just /bin

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ const storage =
 });
 
 console.log(__dirname);
-const pythonPath = '/bin/python3'
-// const pythonPath = path.join(__dirname, '/usr/bin/python3')
+const pythonPath = '/usr/bin/python3'
+// const pythonPath = path.join(__dirname, 'usr/bin/python3')
 
 const upload =
     multer({
