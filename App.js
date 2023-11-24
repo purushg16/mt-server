098bfa0485127b32a8ad345b49d63986ffdbef85
Purushothaman G <macdasys@gmail.com>
2023-11-24 12:38:16 +0530

    python-path /opt/render/.local/in/python3

--- App.js
+++ App.js
@@ -23,7 +23,8 @@ const storage =
 });
 
 console.log(__dirname);
-const pythonPath = '/opt/render/.local/lib/python3'
+const pythonPath = '/opt/render/.local/bin/python3'
+// const pythonPath = '/.venv/bin/python3'
 // const pythonPath = path.join(__dirname, 'usr/bin/python3')
 // /opt/render/.local/lib/python3.7/site-packages
 // /usr/bin/python3
